3208dfee2d5993e530a939b1eb611994c3111fd1
28vikas <63vikas@gmail.com>
2023-08-20 12:21:00 +0530

    list grid

---
+++ List grid/script.js
@@ -0,0 +1,25 @@
+var elements = document.getElementsByClassName("column");
+
+var i;
+
+function listView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "100%";
+  }
+}
+
+function gridView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "50%";
+  }
+}
+
+var container = document.getElementById("btnContainer");
+var btns = container.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
